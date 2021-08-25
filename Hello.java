da6ccee8acb6164f362fceda359c9909ea442852
Kareem-AHosn <89497964+Kareem-AHosn@users.noreply.github.com>
2021-08-24 22:30:18 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("Kareem Aboulhosn");
+	System.out.println("Lab 1");
+	System.out.println("Basic java understanding and function");
+	System.out.println("Created on August 24, 2021");
+	System.out.println("Course 255 Section 004");
+	System.out.println();
+	}
+}
